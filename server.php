bb3af154cb959586c19afcd849237b7f34e69125
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-06-18 16:04:19 -0300

    Autenticacion via HMAC

--- server.php
+++ server.php
@@ -2,6 +2,38 @@
 
 header( 'Content-Type: application/json' );
 
+if ( 
+	!array_key_exists('HTTP_X_HASH', $_SERVER) || 
+	!array_key_exists('HTTP_X_TIMESTAMP', $_SERVER) || 
+	!array_key_exists('HTTP_X_UID', $_SERVER)  
+	) {
+		header( 'Status-Code: 403' );
+	
+		echo json_encode(
+			[
+				'error' => "No autorizado",
+			]
+		);
+		
+		die;
+	}
+
+list( $hash, $uid, $timestamp ) = [ $_SERVER['HTTP_X_HASH'], $_SERVER['HTTP_X_UID'], $_SERVER['HTTP_X_TIMESTAMP'] ];
+$secret = 'Sh!! No se lo cuentes a nadie!';
+$newHash = sha1($uid.$timestamp.$secret);
+
+if ( $newHash !== $hash ) {
+	header( 'Status-Code: 403' );
+	
+		echo json_encode(
+			[
+				'error' => "No autorizado. Hash esperado: $newHash, hash recibido: $hash",
+			]
+		);
+		
+		die;
+}
+
 $allowedResourceTypes = [
 	'books',
 	'authors',
