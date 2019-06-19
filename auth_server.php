84253474be8d12f3e8044414476e60cee7d20a4a
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-06-19 13:34:05 -0300

    Mejora en el manejo de errores HTTP

--- auth_server.php
+++ auth_server.php
@@ -6,30 +6,26 @@ $token = "5d0937455b6744.68357201";
 
 if ( $method === 'POST' ) {
     if ( !array_key_exists( 'HTTP_X_CLIENT_ID', $_SERVER ) || !array_key_exists( 'HTTP_X_SECRET', $_SERVER ) ) {
-        header( 'Status-Code: 400');
+        http_response_code( 400 );
 
-        echo 'Faltan parametros';
-        die;
+        die( 'Faltan parametros' );
     }
 
     $clientId = $_SERVER['HTTP_X_CLIENT_ID'];
     $secret = $_SERVER['HTTP_X_SECRET'];
 
     if ( $clientId !== '1' || $secret !== 'SuperSecreto!' ) {
-        header( 'Status-Code: 403');
+        http_response_code( 403 );
 
-        echo "No autorizado";
-        die;
+        die ( "No autorizado");
     }
 
     echo "$token";
 } elseif ( $method === 'GET' ) {
     if ( !array_key_exists( 'HTTP_X_TOKEN', $_SERVER ) ) {
-        header( 'Status-Code: 400');
+        http_response_code( 400 );
 
-        echo 'Faltan parametros';
-
-        die;
+        die ( 'Faltan parametros' );
     }
 
     if ( $_SERVER['HTTP_X_TOKEN'] == $token ) {
@@ -38,6 +34,5 @@ if ( $method === 'POST' ) {
         echo 'false';
     }
 } else {
-
     echo 'false';
 }
